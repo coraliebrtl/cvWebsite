62856cc29632c2116489f84d852cc4f3ee088511
coraliebrtl <inscription+github@coralie-brt.fr>
2023-09-20 11:55:11 +0200

    feat(project): init project

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Coralie BROTEILLE - Website - CV",
+  description: "Coralie BROTEILLE Website CV",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
